93b5dfdc715a5a753badbaa88be3d1a8311eeaaf
filipediasb <filipe.dbitt@gmail.com>
2022-03-08 11:52:42 -0300

    teste

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        if (Auth::check() === true) {
+            return redirect()->route('admin.home');
+        }
+        return view('admin.index');
+    }
+
+    public function home()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function login(Request $request)
+    {
+        if (in_array('', $request->only('email', 'password'))) {
+            $json['message'] = $this->message->error('Informe todos os dados para efetuar o login')->render();
+            return response()->json($json);
+        }
+
+        if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+            $json['message'] = $this->message->error('Informe um email válido')->render();
+            return response()->json($json);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (!Auth::attempt($credentials)) {
+            $json['message'] = $this->message->error('Usuário e senha não conferem')->render();
+            return response()->json($json);
+        }
+
+        $this->authenticated($request->getClientIp());
+        $json['redirect'] = route('admin.home');
+        return response()->json($json);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('admin.login');
+    }
+
+    protected function authenticated(string $ip)
+    {
+        $user = User::where('id', Auth::user()->id);
+        $user->update([
+            'last_login_at' => date('Y/m/d H:i:s'),
+            'last_login_ip' => $ip
+        ]);
+    }
+}
